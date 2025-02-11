5542aa7d0e14c2af79023744ccc38412f523b09a
Hifzur Rahman <hr493@cam.ac.uk>
2025-02-11 16:22:31 +0000

    Mock text classifier

--- routers/categorize_question.py
+++ routers/categorize_question.py
@@ -1,7 +1,18 @@
 import random 
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
 
-# to implement properly using classifier
+tokenizer = BertTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
+model = BertForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
 def get_category(question: str):
-    categories = list(range(1, 6))
-    return random.choice(categories)
+    with torch.no_grad():
+        inputs = tokenizer(question, return_tensors="pt")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits).item()
+
+    labels = ['very negative', 'negative', 'neutral', 'positive', 'very positive']
+    predicted_label = labels[predicted_class]
+    return predicted_label
