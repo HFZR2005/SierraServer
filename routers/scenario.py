f3ada8db427597a91b20c09fe05fa2bfa347de6c
Hifzur Rahman <hr493@cam.ac.uk>
2025-02-20 12:09:53 +0000

    Changed scenario system prompt

--- routers/scenario.py
+++ routers/scenario.py
@@ -11,11 +11,11 @@ model = "mistral-large-latest"
 
 client = Mistral(api_key=api_key)
 
-prompt_content = """Your task is to generate scenarios for an investigative interviewer to imagine themselves within. The interviewer primarily works with abused children (including sexual), up to age 18. Please generate a scenario of the form: Scenario: [Scenario] Age: [age]. The scenario should be short, include no dialogue and only have a few details. The setting is a court, so the scenario should just be some brief information about evidence. Also include the name and age of the child as separate fields. They cannot be named Lucas.
+prompt_content = """Your task is to generate scenarios for an investigative interviewer to imagine themselves within. The interviewer primarily works with abused children (including sexual), up to age 18. Please generate a scenario of the form: Scenario: [Scenario] Age: [age]. The scenario should be short, include no dialogue and only have a few details. The scenario should just be some brief information about evidence. Also include the name and age of the child as separate fields. They cannot be named Lucas.
 
 Here is an example: 
 
-Scenario: A six-year-old girl named Emily is brought to testify in court regarding allegations of physical abuse. The interviewer is provided with photographs and drawings made by Emily, depicting unexplained bruises and injuries on her body. There are also medical reports detailing multiple fractures, some healing and others still active. A worn teddy bear, belonging to Emily, has been collected as evidence from the home and shows signs of being used as a weapon. The courtroom is filled with tension as Emily bravely faces her accused abuser in the witness box
+Scenario: A six-year-old girl named Emily is brought to testify in court regarding allegations of physical abuse. The interviewer is provided with photographs and drawings made by Emily, depicting unexplained bruises and injuries on her body. There are also medical reports detailing multiple fractures, some healing and others still active. A worn teddy bear, belonging to Emily, has been collected as evidence from the home and shows signs of being used as a weapon.
 
 Name: Emily
 Age: 6 """
