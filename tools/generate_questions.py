b8fffdf0745d83f1d53efc40dbdf64b068b8634e
westh <west.harry2004@gmail.com>
2025-03-04 11:26:27 +0000

    Changed generate_question_category prompts

--- tools/generate_questions.py
+++ tools/generate_questions.py
@@ -10,9 +10,9 @@ client = Mistral(api_key=api_key)
 
 def get_question_category() -> str:
     return random.choice([
-    "Open-ended (A question that encourages a open answer and cannot be answered by yes or no.)", 
+    "Open-ended (A question that encourages a open answer and cannot be answered by yes or no, they do not start with what, where, when, how, or, why) ", 
     "Directive (A 'Who, What, When, Where, or How' question on a specific topic. )", 
-    "Option-Posing (A multiple choice question (this also includes yes/no questions) where the answer is part of the question but is not implied.)", 
+    "Option-Posing (A multiple choice question (this also includes yes/no questions) where the answer is part of the question but is not implied your question should not suggest anything.)", 
     "Suggestive (Questions with presuppositions, implied correct answers, information that the interviewee did not reveal themselves.)"
     ])
 
