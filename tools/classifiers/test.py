958ff7e2664c250c1a1aed5cf24d567f5f6f0238
tasyaa04 <anokhita4@gmail.com>
2025-03-09 15:57:36 +0000

    stage classifier commit

---
+++ tools/classifiers/test.py
@@ -0,0 +1,7 @@
+from classifier import get_question_type, get_stage
+
+text = "Can you please state your full name and tell me how you are related to the matter we're discussing today?"
+
+
+text = "Can you please state your full name and tell me how you are related to the matter we're discussing today?"
+print(get_stage(text))
